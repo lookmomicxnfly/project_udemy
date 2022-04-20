ad7652186b68fa76f0ac230747bd30cf8919f958
Pavel <example@gmail.com>
2022-04-20 10:48:41 +1000

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// alert('Hello');
+
+// let answer = prompt("Yeah?", "Yeah");
+// console.log(answer);
+
+// let answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(answers);
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+
+
+
+// const isChecked = false,
+//       isClose = true;
+
+// console.log(isChecked && isClose);
